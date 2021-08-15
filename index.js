e44941ab21e3fd75507619c25a0a9e0720ccb99e
Yash Nerkar <65919365+yash-png@users.noreply.github.com>
2021-08-15 13:45:50 +0530

    "changed"

--- index.js
+++ index.js
@@ -30,19 +30,19 @@ function saveUserdata(e) {
 
 function getCertificate() {
     // const userName = document.forms["registerationForm"]["username"].value;
-    const firstname = document.forms["registerationForm"]["firstName"].value;
+    let firstname = document.forms["registerationForm"]["firstName"].value;
 
-    const useremail = document.forms["registerationForm"]["email"].value;
+    let useremail = document.forms["registerationForm"]["email"].value;
 
-    const userroll = document.forms["registerationForm"]["roll"].value;
+    let userroll = document.forms["registerationForm"]["roll"].value;
 
-    const userdivision = document.forms["registerationForm"]["division"].value;
+    let userdivision = document.forms["registerationForm"]["division"].value;
 
-    const userName = document.forms["registerationForm"]["username"].value;
+    let userName = document.forms["registerationForm"]["username"].value;
 
     const form = document.querySelector(".form");
     form.setAttribute("style", "display:none");
-    if (userName === firstName + userdivision + userroll) {
+    if (userName === firstname + userdivision + userroll) {
 
         const ifrm = document.createElement("iframe");
         ifrm.setAttribute("src", "lorem-ipsum.pdf");
@@ -53,7 +53,7 @@ function getCertificate() {
 
         const div = document.createElement("div");
         div.setAttribute("h1", "Sorry your ID is not matching");
-        document.body.appendChild(div)
+        document.body.appendChild(div);
 
     }
 
